92a69811c853cf5817c3c6f1b7f921ee55835f5c
Mario Daniel Ruiz Saavedra <mruizd@uninorte.edu.co>
2014-03-02 16:32:19 -0500

    Initial GUI Layout

--- src/fayerbird/core/Autenticador.java
+++ src/fayerbird/core/Autenticador.java
@@ -35,138 +35,135 @@ import twitter4j.conf.ConfigurationBuilder;
  */
 public class Autenticador {
 
-    private Twitter twitter;
-    private ConfigurationBuilder configBuilder;
-    private RequestToken requestToken;
-    private AccessToken accessToken;
-    private File logfile;
+	private Twitter twitter;
+	private ConfigurationBuilder configBuilder;
+	private RequestToken requestToken;
+	private AccessToken accessToken;
+	private File logfile;
 
-    public Autenticador() throws IOException, TwitterException { //Constructor de la clase
-        logfile = new File("auth_file.txt");
-        configBuilder = new ConfigurationBuilder();
-        configBuilder.setDebugEnabled(true);
-        if (logfile.exists()) {
-            try {
-                FileInputStream fileIn = new FileInputStream(logfile);
-                ObjectInputStream objectIn = new ObjectInputStream(fileIn);
-                try {
-                    ArrayList<String> authString = (ArrayList<String>) objectIn.readObject();
-                    configBuilder.setOAuthConsumerKey(authString.get(0));
-                    configBuilder.setOAuthConsumerSecret(authString.get(1));
-                    configBuilder.setOAuthAccessToken(authString.get(2));
-                    configBuilder.setOAuthAccessTokenSecret(authString.get(3));
-                    twitter = new TwitterFactory(configBuilder.build()).getInstance();
-                } finally {
-                    objectIn.close();
-                    this.atEnd();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
+	public Autenticador() throws IOException, TwitterException { //Constructor de la clase
+		logfile = new File("auth_file.txt");
+		configBuilder = new ConfigurationBuilder();
+		configBuilder.setDebugEnabled(true);
+		if (logfile.exists()) {
+			try {
+				FileInputStream fileIn = new FileInputStream(logfile);
+				ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+				try {
+					ArrayList<String> authString = (ArrayList<String>) objectIn.readObject();
+					configBuilder.setOAuthConsumerKey(authString.get(0));
+					configBuilder.setOAuthConsumerSecret(authString.get(1));
+					configBuilder.setOAuthAccessToken(authString.get(2));
+					configBuilder.setOAuthAccessTokenSecret(authString.get(3));
+					twitter = new TwitterFactory(configBuilder.build()).getInstance();
+				} finally {
+					objectIn.close();
+					this.atEnd();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
 
-            configBuilder.setOAuthConsumerKey("DI05nv99UI3wSSlqI6qJaA");
-            configBuilder.setOAuthConsumerSecret("P3RVFt0q11edhdEuwRIJRead13F7orXLjwawjRatW2k");
-            twitter = new TwitterFactory(configBuilder.build()).getInstance();
+			configBuilder.setOAuthConsumerKey("DI05nv99UI3wSSlqI6qJaA");
+			configBuilder.setOAuthConsumerSecret("P3RVFt0q11edhdEuwRIJRead13F7orXLjwawjRatW2k");
+			twitter = new TwitterFactory(configBuilder.build()).getInstance();
 
-            requestToken = null;
-            accessToken = null;
-            String url = null;
-            try {
-                requestToken = twitter.getOAuthRequestToken();
-                System.out.println("Request Tokens obtenidos con éxito.");
-                System.out.println("Request Token: " + requestToken.getToken());
-                System.out.println("Request Token secret: " + requestToken.getTokenSecret());
-                url = requestToken.getAuthorizationURL();
-                System.out.println("URL:");
-                System.out.println(url);
-            } catch (TwitterException ex) {
-                Logger.getLogger(Fayerbird.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-            if (Desktop.isDesktopSupported()) {
-                Desktop desktop = Desktop.getDesktop();
-                try {
-                    desktop.browse(new URI(url));
-                } catch (Exception e) {
+			requestToken = null;
+			accessToken = null;
+			String url = null;
+			try {
+				requestToken = twitter.getOAuthRequestToken();
+				System.out.println("Request Tokens obtenidos con éxito.");
+				System.out.println("Request Token: " + requestToken.getToken());
+				System.out.println("Request Token secret: " + requestToken.getTokenSecret());
+				url = requestToken.getAuthorizationURL();
+				System.out.println("URL:");
+				System.out.println(url);
+			} catch (TwitterException ex) {
+				Logger.getLogger(Fayerbird.class.getName()).log(Level.SEVERE, null, ex);
+			}
+			BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+			if (Desktop.isDesktopSupported()) {
+				Desktop desktop = Desktop.getDesktop();
+				try {
+					desktop.browse(new URI(url));
+				} catch (Exception e) {
 
-                    e.printStackTrace();
-                }
-            } else {
-                Runtime runtime = Runtime.getRuntime();
-                try {
-                    runtime.exec("xdg-open " + url);
-                } catch (IOException e) {
+					e.printStackTrace();
+				}
+			} else {
+				Runtime runtime = Runtime.getRuntime();
+				try {
+					runtime.exec("xdg-open " + url);
+				} catch (IOException e) {
 
-                    e.printStackTrace();
-                }
-            }
-            
-            PinVentana vent = new PinVentana(this);
-            vent.setVisible(true);
+					e.printStackTrace();
+				}
+			}
 
-            
-        }
+			PinVentana vent = new PinVentana(this);
+			vent.setVisible(true);
 
-       
 
-    }
+		}
 
-    public ConfigurationBuilder getConfigBuilder() {
-        return configBuilder;
-    }
-    public void escribirArchivo(){
-        try {
-                OutputStream file = new FileOutputStream(logfile);
-                OutputStream buffer = new BufferedOutputStream(file);
-                ObjectOutput output = new ObjectOutputStream(buffer);
-                try {
-                    ArrayList<String> auth = new ArrayList<String>();
-                    auth.add("DI05nv99UI3wSSlqI6qJaA");
-                    auth.add("P3RVFt0q11edhdEuwRIJRead13F7orXLjwawjRatW2k");
-                    auth.add(accessToken.getToken());
-                    auth.add(accessToken.getTokenSecret());
-                    output.writeObject(auth);
-                } finally {
-                    output.close();
-                }
 
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-    }
 
-    public File getLogfile() {
-        return logfile;
-    }
-    
-    
-    
-    public Twitter getTwitter() {
-        return twitter;
-    }
+	}
 
-    public AccessToken getAccessToken() {
-        return accessToken;
-    }
+	public ConfigurationBuilder getConfigBuilder() {
+		return configBuilder;
+	}
 
-    public void setAccessToken(AccessToken accessToken) {
-        this.accessToken = accessToken;
-        twitter.setOAuthAccessToken(this.accessToken);
-    }
+	public void escribirArchivo() {
+		try {
+			OutputStream file = new FileOutputStream(logfile);
+			OutputStream buffer = new BufferedOutputStream(file);
+			ObjectOutput output = new ObjectOutputStream(buffer);
+			try {
+				ArrayList<String> auth = new ArrayList<String>();
+				auth.add("DI05nv99UI3wSSlqI6qJaA");
+				auth.add("P3RVFt0q11edhdEuwRIJRead13F7orXLjwawjRatW2k");
+				auth.add(accessToken.getToken());
+				auth.add(accessToken.getTokenSecret());
+				output.writeObject(auth);
+			} finally {
+				output.close();
+			}
 
-    public RequestToken getRequestToken() {
-        return requestToken;
-    }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    public void setRequestToken(RequestToken requestToken) {
-        this.requestToken = requestToken;
+	public File getLogfile() {
+		return logfile;
+	}
 
-    }
-    
-    public void atEnd(){
-        new Ventana(this).setVisible(true);
-    }
-    
-    
+	public Twitter getTwitter() {
+		return twitter;
+	}
+
+	public AccessToken getAccessToken() {
+		return accessToken;
+	}
+
+	public void setAccessToken(AccessToken accessToken) {
+		this.accessToken = accessToken;
+		twitter.setOAuthAccessToken(this.accessToken);
+	}
+
+	public RequestToken getRequestToken() {
+		return requestToken;
+	}
+
+	public void setRequestToken(RequestToken requestToken) {
+		this.requestToken = requestToken;
+
+	}
+
+	public void atEnd() {
+		new Ventana(this).setVisible(true);
+	}
 }
--- src/fayerbird/core/Fayerbird.java
+++ src/fayerbird/core/Fayerbird.java
@@ -16,42 +16,42 @@ import twitter4j.TwitterException;
  */
 public class Fayerbird {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        try {
-            Autenticador auth = new Autenticador();
-            
-        } catch (IOException ex) {
-            Logger.getLogger(Fayerbird.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (TwitterException ex) {
-            Logger.getLogger(Fayerbird.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		try {
+			Autenticador auth = new Autenticador();
+
+		} catch (IOException ex) {
+			Logger.getLogger(Fayerbird.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (TwitterException ex) {
+			Logger.getLogger(Fayerbird.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		/* Set the Nimbus look and feel */
+		//<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-     
-                
-        
-    }
+		 * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+		 */
+		try {
+			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					javax.swing.UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (ClassNotFoundException ex) {
+			java.util.logging.Logger.getLogger(Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (InstantiationException ex) {
+			java.util.logging.Logger.getLogger(Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (IllegalAccessException ex) {
+			java.util.logging.Logger.getLogger(Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
+			java.util.logging.Logger.getLogger(Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		}
+		//</editor-fold>
+
+
+
+	}
 }
